8c4b0764a9cf1efb3fc6eaf664210ce2c6376f0a
sedong chun <sdcheon@lfpartners.co.kr>
2020-08-05 01:20:08 +0900

    킷테스ㅌ

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,13 +1,20 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import org.hibernate.mapping.Collection;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import java.util.List;
+
+import static jpabook.jpashop.domain.OrderStatus.CANCEL;
+import static jpabook.jpashop.domain.OrderStatus.ORDER;
 
 public class JpaMain {
 
@@ -18,12 +25,54 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Book book = new Book();
-            book.setName("JPA");
-            book.setAuthor("김영한");
-            em.persist(book);
+
+
+            Order orderA = new Order();
+            orderA.setStatus(ORDER);
+            em.persist(orderA);
+            Order orderB = new Order();
+            orderB.setStatus(CANCEL);
+            em.persist(orderB);
+            Order orderC = new Order();
+            orderC.setStatus(ORDER);
+            em.persist(orderC);
+
+            OrderItem orderItemA = new OrderItem();
+            orderItemA.setOrderPrice(2000);
+            orderItemA.setOrder(orderA);
+            em.persist(orderItemA);
+
+            OrderItem orderItemB = new OrderItem();
+            orderItemB.setOrderPrice(1000);
+            orderItemB.setOrder(orderA);
+            em.persist(orderItemB);
+
+            OrderItem orderItemC = new OrderItem();
+            orderItemC.setOrderPrice(1000);
+            orderItemC.setOrder(orderB);
+            em.persist(orderItemC);
+
+            em.flush();
+            em.clear();
+
+            String query = "select o from OrderItem o join fetch o.order";
+
+            List<OrderItem> result = em.createQuery(query,OrderItem.class).getResultList();
+            //List<Member> result = em.createQuery("select m from Member m where m.name like '%k%' ",Member.class).getResultList();
+            for(OrderItem orderItem : result){
+                System.out.println("orderItem = "+orderItem.getId());
+            }
+           // String query = "select oi from Order o join o.orderItems oi";
+
+//            List<Order> result = em.createQuery(query,Order.class).getResultList();
+//            //List<Member> result = em.createQuery("select m from Member m where m.name like '%k%' ",Member.class).getResultList();
+//            for(Order order : result){
+//                System.out.println("orderItem = "+order.getId());
+//            }
+            System.out.println("result="+result);
             tx.commit();
         }catch (Exception e) {
+            System.out.println(e.toString());
             tx.rollback();
         }finally {
             em.close();
